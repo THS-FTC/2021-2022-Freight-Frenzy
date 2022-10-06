f7a85bc1fcfb7dc3da1e84082b974c8bd3a90b2e
Kai Song <95108448+KSO2525@users.noreply.github.com>
2022-10-06 11:49:09 -0400

    Add files via upload

---
+++ 19477/oneRed.java
@@ -0,0 +1,232 @@
+/*
+- 1 is first
+Controls:
+    Gamepad 1 (all random functions):
+        left BUMPER BUTTON - reset all carousel and intake servos
+        right BUMPER BUTTON - turn on the carousel motor
+        left TRIGGER - intake forward
+        right TRIGGER - intake backward
+        x button - arm 1st position
+        a button - arm 2nd position
+        b button - arm 3rd position
+    Gamepad 2 (drive):
+        left analog stick - all controls that make logical
+        right analog stick - radial turns in logical order and drift
+        right BUMPER BUTTON - turn all motors off
+
+Point sequence
+    1st part:
+        score freight to shipping hub level 3
+    End Game:
+        deliver all ducks
+        fully park in warehouse
+        if extra time:
+            ***try to have shared shipping hub touching floor on our side
+
+- Servo_3 = claw tilt
+- Servo_4 = claw open/close
+*/
+
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.robotcore.hardware.Light;
+import com.qualcomm.robotcore.hardware.CRServo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+//import com.vuforia.Vuforia;
+//import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
+@TeleOp
+
+public class oneRed extends LinearOpMode {
+    //changeable variables
+    double carousel_speed = 1.0;
+    double high_pos = 110.0;
+    double mid_pos = 85.0;
+    double low_mid = 65.0;
+    double down = 0.0;
+    //junction height values represented as motor encoder values for 4-stage Viper Slide Kit
+    int groundJunction = 600;
+    int lowJunction = 2900;
+    int midJunction = 5400;
+    int highJunction = 5400;
+    double slideSpeed = 3000.0;
+    int armTarget = 0;//as encoder values
+    int slidePosition = 0;
+    boolean calibrated = false;
+    //hardware classes + names
+    private Blinker Control_Hub;//NEEDED - DON'T DELETE!!
+    private DcMotor Motor_1;//front left
+    private DcMotor Motor_2;//front right
+    private DcMotor Motor_3;//back left
+    private DcMotor Motor_4;//back right
+    //private DcMotorEx armMotor;
+    private Gyroscope imu;
+    //private Servo clawServo;
+    //motor variables for mecanum drive
+    double motor_reduction = 0.4;//for drivetrain
+    double motor_1_pwr;
+    double motor_2_pwr;
+    double motor_3_pwr;
+    double motor_4_pwr;
+    double motor_denom;
+    //inputs
+    double left_stick2_x;//triggers and bumpers
+    double left_stick2_y;
+    double right_stick2_x;
+    double left_stick1_y;
+    boolean left_bump1;
+    boolean right_bump1;
+    boolean left_bump2;
+    boolean right_bump2;
+    double left_trig1;
+    double right_trig1;
+    double left_trig2;
+    double right_trig2;
+    boolean a1;//a,b,x,y buttons
+    boolean b1;
+    boolean x1;
+    boolean y1;
+    boolean a2;
+    boolean b2;
+    boolean x2;
+    boolean y2;
+    boolean dpad_left;
+    boolean dpad_down;
+    boolean dpad_up;
+    boolean dpad_right;
+
+    @Override
+    public void runOpMode() {
+        //hardware intializing
+        Control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
+        Motor_1 = hardwareMap.get(DcMotor.class, "Motor_1");
+        Motor_2 = hardwareMap.get(DcMotor.class, "Motor_2");
+        Motor_3 = hardwareMap.get(DcMotor.class, "Motor_3");
+        Motor_4 = hardwareMap.get(DcMotor.class, "Motor_4");
+        //armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
+        //clawServo = hardwareMap.get(Servo.class, "clawServo");//to move the claw, grab the cone.
+        //other things
+        Motor_1.setDirection(DcMotorSimple.Direction.REVERSE);
+        Motor_3.setDirection(DcMotorSimple.Direction.REVERSE);
+        Motor_2.setDirection(DcMotorSimple.Direction.FORWARD);
+        Motor_4.setDirection(DcMotorSimple.Direction.FORWARD);
+        Motor_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor_2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor_3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor_4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //imu = hardwareMap.get(Gyroscope.class, "imu");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        /*if (calibrated == false){//calibrate slide motor to encoder val = 0
+            slideCalibrate();
+            calibrated = true;
+        }*/
+        waitForStart();
+
+        //main loop
+        while (opModeIsActive()) {
+            normal_motor();
+            //claw();
+            //team_2_arm();
+            //telemetry.addData("motor1", left_stick2_y);
+            telemetry.update();
+        }
+
+    }
+
+
+
+
+
+    void normal_motor(){//normal motor control maths + telemetry
+        //bumpers
+        left_bump2 = this.gamepad2.left_bumper;
+        right_bump2 = this.gamepad2.right_bumper;
+        //chassis control
+        if (right_bump2 == true){//slow
+            motor_reduction = 0.2;
+        }
+        else if (left_bump2 == true){//fast
+            motor_reduction = 0.4;
+        }
+
+        //drivetrain
+        right_stick2_x = this.gamepad2.right_stick_x;
+        left_stick2_x = this.gamepad2.left_stick_x;
+        left_stick2_y = -this.gamepad2.left_stick_y;
+        motor_denom = Math.max(Math.abs(left_stick2_y) + Math.abs(left_stick2_x) + Math.abs(right_stick2_x), 1.0);
+        motor_1_pwr = (left_stick2_y + left_stick2_x + right_stick2_x)/motor_denom;//LF
+        motor_2_pwr = (left_stick2_y - left_stick2_x - right_stick2_x)/motor_denom;//RF
+        motor_3_pwr = (left_stick2_y - left_stick2_x + right_stick2_x)/motor_denom;//LB
+        motor_4_pwr = (left_stick2_y + left_stick2_x - right_stick2_x)/motor_denom;//LR
+        Motor_1.setPower(motor_1_pwr  * motor_reduction);
+        Motor_2.setPower(motor_2_pwr  * motor_reduction);
+        Motor_3.setPower(motor_3_pwr  * motor_reduction);
+        Motor_4.setPower(motor_4_pwr  * motor_reduction);
+        //telemetry.addData("motor_1", motor_1_pwr);
+        //telemetry.addData("motor_2", motor_2_pwr);
+        //telemetry.addData("motor_3", motor_3_pwr);
+        //telemetry.addData("motor_4", motor_4_pwr);
+        telemetry.addData("encoder-left", Motor_1.getCurrentPosition());
+        telemetry.addData("encoder-mid", Motor_2.getCurrentPosition());
+        telemetry.addData("encoder-right", Motor_3.getCurrentPosition());
+        telemetry.update();
+    }
+
+    void slideCalibrate(){
+        armMotor.setPower(-0.75);
+        if (armMotor.getVelocity(AngleUnit.DEGREES) <= 2){
+            armMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);//reset encoder of slideMotor when slide is fully retracted
+        }
+    }
+
+    void claw(){
+        a2 = this.gamepad2.a;
+        x2 = this.gamepad2.x;
+        if (a2 == true){
+            clawServo.setPosition(30.0/270.0);//position for OPEN CLAW
+
+        }
+        else if (x2 == true){
+            clawServo.setPosition(100.0/270.0);//position for CLOSED CLAW
+        }
+    }
+
+    void team_2_arm(){//team 2 arm design with claw and 2 servos
+        left_trig2 = this.gamepad2.left_trigger;
+        right_trig2 = -this.gamepad2.right_trigger;
+        if(left_trig2 != 0.0 && armTarget > 0){
+            armTarget -= 5;
+            armMotor.setTargetPosition(armTarget);
+        }
+        else if(right_trig2 != 0.0){
+            armTarget -= 5;
+            armMotor.setTargetPosition(armTarget);
+        }
+        else{
+            armMotor.setPower(0.0);
+        }
+        telemetry.addData("armPos: ", armTarget);
+        telemetry.update();
+    }*/
+}
+
+
